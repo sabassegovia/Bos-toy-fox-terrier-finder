2efda227cd0e89f2f07e634465e3abbc712b48a3
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-13 00:52:54 -0700

    completed skeleton for about section w/ photo

--- bos-toy-fox-terrier-finder/src/pages/About.js
+++ bos-toy-fox-terrier-finder/src/pages/About.js
@@ -1,7 +1,10 @@
+import aboutPhoto from '../images/aboutphoto.png';
+
 const About = () => {
   return (
     <div className="about">
       <h1>About <span className="theguy">The Guy</span></h1>
+      <img src={aboutPhoto} alt="about"/>
       <p>Hi! I'm the guy! Come back later! Heck!</p>
       <p>Unless you have snacks, then you can stay!</p>
     </div>
